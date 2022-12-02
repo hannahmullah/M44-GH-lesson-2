52e6252be57adc610eb89dfa2e423ac04f277895
Hannah Mullah <hannahmullah@hotmail.com>
2022-12-02 15:41:35 +0000

    commited gey keywords file as was not tracked

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const printFruit = () => {
 
 printFruit();
 
+/* key git terms
+git status
+git add .
+git log
+git commit -m"comment"
+git log --online --graph
+git push
+git switch -c nameOfNewBranch
+git pull
+git push --set-upstream origin nameOfNewBranch
+git switch -> to swtich to any other branch */
